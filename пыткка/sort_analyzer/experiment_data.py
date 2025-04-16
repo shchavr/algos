abc293ded90be4c8271a0d727a5a9af65ab787d9
Polina Shchavrovskaia <146020791+shchavr@users.noreply.github.com>
2025-04-16 22:56:08 +0500

    Delete пыткка directory

--- пыткка/sort_analyzer/experiment_data.py
+++
@@ -1,7 +0,0 @@
-class ExperimentData:
-    """
-    Структура для хранения данных эксперимента.
-    """
-    def __init__(self, length, counts):
-        self.length = length
-        self.counts = counts
